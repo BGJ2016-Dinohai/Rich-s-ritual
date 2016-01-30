cddd56c4de5ef717fab905a60f6789a598215382
Haakon Løtveit <haakon_lotveit@live.no>
2016-01-30 12:25:09 +0100

    Fjernet load level fra movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,55 +7,14 @@ public class Movement : MonoBehaviour {
 
     public TextAsset text;
     public float moveSpeed = 0.1f;
-    public Transform[] tiles;
     
     Vector2 moveVector;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log(Application.dataPath);
-        string csvText = text.text.Replace("\r", "");
-
-        var lineNumber = 0;
-        foreach(string line in csvText.Split('\n'))
-        {            
-            var columnNumber = 0;
-            foreach (string column in line.Split(','))
-            {
-                addTile(columnNumber, lineNumber, column);
-                ++columnNumber;
-            }
-            ++lineNumber;
-        }
   
 	}
-
-    private void addTile(int xPosition, int yPosition, string tileName)
-    {
-        var tile = LookUpTile(tileName);
-        Debug.Log(string.Format("Should make tile of type {0} at <{1},{2}>", tile, xPosition, yPosition));
-    }
-
-    private string LookUpTile(string tileName)
-    {
-        Debug.Log(tileName == null);
-
-        string retVal = "ERROR";
-        switch(tileName)
-        {
-            case "s": retVal = "Start"; break;
-            case "m": retVal = "Mål"; break;
-            case "n": retVal = "Nøkkel"; break;
-            case "d": retVal = "Dør"; break;
-            case "v": retVal = "Vegg"; break;
-            case "l": retVal = "Lava"; break;
-            case "g": retVal = "Gulv"; break;
-            case "0": retVal = "Utenfor"; break;
-            case "": retVal = "Gulv"; break;            
-            default: throw new Exception(string.Format("tile specifier {0} {1} is not valid", tileName, Convert.ToInt32(tileName.ToCharArray()[0])));
-        }
-        return retVal;
-    }
+    
     // Update is called once per frame
     void Update () {
         moveVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));

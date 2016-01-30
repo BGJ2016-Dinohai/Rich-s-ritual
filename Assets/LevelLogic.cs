f1dfafdcdd752a40d38651d08e79f91cd2928854
Haakon Løtveit <haakon_lotveit@live.no>
2016-01-30 14:11:08 +0100

    Laget levekontroller sentrerer kamera på midten av brettet

---
+++ Assets/LevelLogic.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLogic : MonoBehaviour {
+
+    string[][] level;
+    private bool hasKey;
+
+    public void setLevel(string[][] level)
+    {
+        this.level = level;
+    }
+
+    public void createEmptyLevel(int width, int height)
+    {
+        level = new string[width][];
+        for(int rowNr = 0; rowNr < width; ++rowNr)
+        {
+            level[rowNr] = new string[height];          
+        }
+    }
+
+    public void setTile(int xPosition, int yPosition, string tile)
+    {
+        level[xPosition][yPosition] = tile;
+    }
+
+	// Use this for initialization
+	void Start () {
+        createEmptyLevel(16, 16);
+	}
+	
+    public void pickedUpKey()
+    {
+        this.hasKey = true;
+    }
+
+    public bool canWalk(int x, int y)
+    {
+        bool retVal = false;
+        switch(level[x][y])
+        {
+            case "":
+            case "g":
+            case "l":
+            case "n":
+            case "s":
+            case "m":
+                retVal = true; break;
+            case "d":
+                retVal = hasKey; break;
+            default:
+                retVal = false; break;
+        }
+        return retVal;
+    }
+
+    public bool isDeadly(int x, int y)
+    {
+        return level[x][y].Equals("l");
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/LoadLevel.cs
+++ Assets/LoadLevel.cs
@@ -3,19 +3,34 @@ using System.Collections;
 using System;
 
 public class LoadLevel : MonoBehaviour {
+    public LevelLogic levelController;
 	public TextAsset text;
+    public Transform wallTile;
+    public Transform floorTile;
+    public Transform lavaTile;
+    public Transform doorTile;
+    public Transform goalTile;
+    public Transform startTile;
+    public Transform keyTile;
+    public float gridSize = 0.65f;
 
 	// Use this for initialization
 	void Start () {
+        if (null == levelController)
+        {
+            levelController = GetComponent<LevelLogic>();
+        }
+
 		Debug.Log(Application.dataPath);
 
 		/* Windows-proofing the text. We assume no OS9 or earlier. *crosses fingers* */
 		string csvText = text.text.Replace("\r", "");
 
 		var lineNumber = 0;
-		foreach(string line in csvText.Split('\n'))
-		{            
-			var columnNumber = 0;
+        var columnNumber = 0;
+        foreach (string line in csvText.Split('\n'))
+		{
+            columnNumber = 0;
 			foreach (string column in line.Split(','))
 			{
 				addTile(columnNumber, lineNumber, column);
@@ -23,32 +38,31 @@ public class LoadLevel : MonoBehaviour {
 			}
 			++lineNumber;
 		}
+        GameObject.FindGameObjectWithTag("MainCamera").transform.position = new Vector3(columnNumber / 2 * gridSize, -lineNumber / 2 * gridSize, 0);
 	}
 
 	private void addTile(int xPosition, int yPosition, string tileName)
 	{
 		var tile = LookUpTile(tileName);
 		Debug.Log(string.Format("Should make tile of type {0} at <{1},{2}>", tile, xPosition, yPosition));
+        Instantiate(tile, new Vector3(xPosition * gridSize, -yPosition * gridSize, 0), Quaternion.identity);
+        levelController.setTile(xPosition, yPosition, tileName);
 	}
 	
-	private string LookUpTile(string tileName)
+	private Transform LookUpTile(string tileName)
 	{
-		Debug.Log(tileName == null);
-		
-		string retVal = "ERROR";
+        Transform retVal;
 		switch(tileName)
 		{
-		case "s": retVal = "Start"; break;
-		case "m": retVal = "Mål"; break;
-		case "n": retVal = "Nøkkel"; break;
-		case "d": retVal = "Dør"; break;
-		case "v": retVal = "Vegg"; break;
-		case "l": retVal = "Lava"; break;
-		case "g": retVal = "Gulv"; break;
-		case "0": retVal = "Utenfor"; break;
-		case "": retVal = "Utenfor"; break;
-		case "\r":retVal = "SLASH R ! ;_;"; break;
-		case null: retVal = "NULL"; break;
+		case "s": retVal = startTile; break;
+		case "m": retVal = goalTile; break;
+		case "n": retVal = keyTile; break;
+		case "d": retVal = doorTile; break;
+		case "v": retVal = wallTile; break;
+		case "l": retVal = lavaTile; break;
+		case "g": retVal = floorTile; break;
+		case "0": retVal = wallTile; break;
+		case "": retVal = floorTile; break;
 		default: throw new Exception(string.Format("tile specifier {0} {1} is not valid", tileName, Convert.ToInt32(tileName.ToCharArray()[0])));
 		}
 		return retVal;

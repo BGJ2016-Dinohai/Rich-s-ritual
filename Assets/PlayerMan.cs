5ad40eb9c7c00398a8da6714d0748c6d433b99d1
Dan-Eric Næss <dan-eric.naess@live.no>
2016-01-30 13:38:46 +0100

    Spaghetti! He moves now

--- Assets/PlayerMan.cs
+++ Assets/PlayerMan.cs
@@ -13,6 +13,7 @@ public class PlayerMan : MonoBehaviour {
     private string currentSeq;
 
     private IDictionary<string, string> moveList;
+    private IDictionary<char, Vector3> moveVectors;
 
     // Use this for initialization
     void Start () {
@@ -22,6 +23,11 @@ public class PlayerMan : MonoBehaviour {
 
         // oh jesus
         moveList = new Dictionary<string, string>();
+        moveVectors = new Dictionary<char, Vector3>();
+        moveVectors['u'] = new Vector3(0, 1, 0);
+        moveVectors['d'] = new Vector3(0, -1, 0);
+        moveVectors['l'] = new Vector3(-1, 0, 0);
+        moveVectors['r'] = new Vector3(1, 0, 0);
 
         string moves = moveText.text.Replace("\r","");
         foreach(string line in moves.Split('\n'))
@@ -42,8 +48,54 @@ public class PlayerMan : MonoBehaviour {
         return retVal;
     }
 
+    private bool moving = false;
+    private float tween = 0;
+    private string pattern;
+    private Vector3 oldPos;
+    void move(string p = null)
+    {
+        if(p != null)
+        {
+            moving = true;
+            pattern = p;
+            oldPos = ply.position;
+        }
 
-	void Update () {
+        tween += Time.deltaTime;
+        float smooth = smoothstep(0.0f, 1.0f, tween);
+        if (pattern.Length > 0)
+        {
+            ply.position = oldPos + moveVectors[pattern[0]] * smooth;
+        }
+
+
+        if (tween > 1)
+        {
+            tween = 0;
+            oldPos = ply.position;
+            pattern = pattern.Substring(1);
+            if(pattern == "")
+            {
+                moving = false;
+            }
+        }
+    }
+
+    private float smoothstep(float edge0, float edge1, float x)
+    {
+        x = Mathf.Clamp((x - edge0) / (edge1 - edge0), 0.0f, 1.0f);
+        return x * x * (3 - 2 * x);
+    }
+
+
+	void Update ()
+    {
+        if (moving)
+        {
+            move();
+            return;
+        }
+
         if (!Input.anyKeyDown){ return; }
         if (currentSeq.Length == 4)
         {
@@ -60,6 +112,7 @@ public class PlayerMan : MonoBehaviour {
                 if (moveList.TryGetValue(currentSeq, out value))
                 {
                     Debug.Log(string.Format("Move like this: {0}", value));
+                    move(value);
                 }
             }
         }

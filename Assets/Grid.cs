147b67d763416ebc26c84045796a3a225e457eba
Dan-Eric Næss <dan-eric.naess@live.no>
2016-01-30 12:49:00 +0100

    Alt i min asset mappe

---
+++ Assets/Grid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Grid : MonoBehaviour {
+
+    public Transform walkable;
+    public Transform lava;
+    public Sprite tileSprite;
+
+    // Use this for initialization
+    void Start () {
+        
+        float spriteHeight = tileSprite.bounds.size.y;
+        for (int j = 0; j < 10; j++)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Transform clone = Instantiate(walkable) as Transform;
+                clone.Translate(i * spriteHeight, j * spriteHeight, 0.0f);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}

6818c23cf7da3e17a8d9c5c1840aa32b56c10662
Dan-Eric Næss <dan-eric.naess@live.no>
2016-01-31 13:01:33 +0100

    Quick and dirty levelmanager

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public TextAsset[] levels;
+
+    public static TextAsset[] sLevelList;
+    public static int levelIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        sLevelList = levels;
+        levelIndex = 0;
+    }
+
+    public static void nextLevel(int level = -1)
+    {
+        if(level > -1)
+        {
+            levelIndex = level;
+        }
+
+        if(levelIndex >= sLevelList.Length)
+        {
+            Application.LoadLevel("mainmenu");
+        }
+
+        LoadLevel.nextLevel = sLevelList[levelIndex];
+        levelIndex++;
+        Application.LoadLevel("LevelLoadingScene");
+    }
+
+    public static void reloadLevel()
+    {
+        LoadLevel.nextLevel = sLevelList[levelIndex-1];
+        Application.LoadLevel("LevelLoadingScene");
+    }
+}
